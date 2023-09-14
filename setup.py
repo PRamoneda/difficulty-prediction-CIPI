35835fdd4c731aab1398225271a1000202b4f449
PRamoneda <pramoneda@github.com>
2023-09-14 09:24:38 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     url='https://github.com/pramoneda/difficulty-prediction-CIPI',
     package_data={
         'eswa_difficulty.piano_fingering': ['models/*.pth'],
-        'eswa_difficulty': ['eswa_models/*.pth', 'virtuosoNet/checkpoint_last.pt'],
+        'eswa_difficulty': ['eswa_models/*.pth', 'virtuosoNet/*'],
     },
     classifiers=[
         'License :: OSI Approved :: GNU Affero General Public License v3',
